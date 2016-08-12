f07b569e8409fe12cd3d49bbfe925de321f562c5
vignesh-g561 <vgowrabathuni@gmail.com>
2016-08-12 12:59:05 +0530

    Add files via upload

--- Client/src/js/components/Metrics.js
+++ Client/src/js/components/Metrics.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var Navbar=require('./Navbar');
+var BarGraph=require('../pages/BarGraph');
 var Metrics = React.createClass({
     render:function(){
         return (
           	<div>
             <Navbar />
             <center><h1>Metrics Page</h1></center>
+            <BarGraph />
           	</div>
         );
     }
