faa565be021de144d8ee315128b96880ec5e2665
vignesh-g561 <vgowrabathuni@gmail.com>
2016-08-12 12:56:28 +0530

    Add files via upload

--- Client/src/js/components/Profile.js
+++ Client/src/js/components/Profile.js
@@ -28,8 +28,39 @@ var Profile = React.createClass({
     return (
       <div>
         <Navbar />
-        <center><h1>Profile</h1></center>
-        <h3>{JSON.stringify(this.state.eData)}</h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+            <div className="panel panel-default">
+              <div className="panel-heading"><h2>User profile</h2></div>
+              <div className="panel-body">
+                <div className="row">
+                  <div className="col-md-4 text-center">
+                    <img className="img-circle avatar avatar-original" src="../../images/user.png" alt="user image" />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="row">
+                      <div className="col-md-12">
+                        <h1 className="only-bottom-margin">{this.state.eData["Emp Name"]}</h1>
+                      </div>
+                    </div>
+                    <div className="row">
+                      <div className="col-md-6">
+                        <span className="text-muted">Employee Id:</span> {this.state.eData["Emp No"]}<br />
+                        <span className="text-muted">Technology/Skills:</span> {this.state.eData["Technology/Skills"]}<br />
+                        <span className="text-muted">Digithon Cleared? (Y/N/NA):</span> {this.state.eData["Digithon Cleared? (Y/N/NA)"]}<br />
+                        <span className="text-muted">Digital Academy Complete? (Y/N/NA):</span> {this.state.eData["Digital Academy Complete? (Y/N/NA)"]}<br />
+                        <span className="text-muted">Agile Trainings Complete? (Y/N):</span> {this.state.eData["Agile Trainings Complete? (Y/N)"]}<br />
+                        <span className="text-muted">BFSI Training Courses Complete? (Y/N):</span> {this.state.eData["BFSI Training Courses Complete? (Y/N)"]}<br />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
